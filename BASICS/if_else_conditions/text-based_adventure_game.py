d9ffa88e4964e4c53772d69523584302daa42e2b
Terrence <terrenceliang1330@gmail.com>
2026-05-03 16:04:26 -0400

    right path polished

--- BASICS/if_else_conditions/text-based_adventure_game.py
+++ BASICS/if_else_conditions/text-based_adventure_game.py
@@ -31,7 +31,7 @@ if leftOrRight == "left":
                     print("Hmm? Think again. You get crushed by the falling ceiling in the process of indecision. Pathetic.")
 
             else:
-                print("Hmm? Think again. You die in the process of indecision.")
+                print("Hmm? Think again. You get crushed by the falling ceiling in the process of indecision. Pathetic.")
 
         elif headOrLegs == "legs":
             print("You slice the beast's legs off, but something goes wrong. The beast regenates its legs. You didn't know that it could regerate. You die in the hands of the beast due to lack of knowledge. Pathetic.")
@@ -50,3 +50,41 @@ if leftOrRight == "left":
 
 
 
+
+elif leftOrRight == "right":
+    print("You venture into the mysterious cave. The air is damp and the darkness is suffocating. As you explore deeper, you see two glowing eyes staring at you; a dragon. Do you stab it in the eyes or try to sneak past it?")
+    hideOrAttack = input("Do you hide from the dragon or attack it?").lower()
+
+    if hideOrAttack == "hide":
+        print("You try to desolve into the shadows and hide from the dragon, but it has a keen sense of smell. It detects your presence and breathes fire in your direction. You are burned to ashes in an instant. Coward.")
+
+    elif hideOrAttack == "attack":
+        print("You charge at the dragon's eyes and strike with your sword. The dragon howls in pain and falls to the ground, defeated. You continue on but the cave starts to collapse. You run as fast as you can, but you come across a fork in the path. Do you go left or right?")
+        leftOrRight2 = input("Should you go to the left, or to the right?").lower()
+
+        if leftOrRight2 == "left":
+            print("You take the left path and find a hidden chamber. Inside, you discover that it is a dead end. You turn back but the cave has collapsed behind you. You are trapped and eventually die of starvation. Pathetic.")
+
+        elif leftOrRight2 == "right":
+            print("You take the right path and find an opening that leads you outside. You have escaped the collapsing cave and continue on your quest for the Sword of Destiny!")
+            ignoreOrTalkWitch = input("As you continue on, you come across a witch. Do you ignore her or talk to her?").lower()
+
+            if ignoreOrTalkWitch == "ignore":
+                print("You ignore the witch and continue on your quest. However, the witch is offended by your rudeness and casts a spell on you, but before she can finish the spell, you draw your sword and stab her in the heart. The witch dies and you continue on your quest. You then walk upon a rock with the Sword of Destiny on it. You pick up the sword and awaken its power, becoming the greatest knight in history. Horray!")
+
+            elif ignoreOrTalkWitch == "talk":
+              print("You talk to the witch and she seems friendly. She offers you help but she then throws a potion of burning acid at you. You die in agony. Pathetic.")
+
+            else:
+                print("Hmm? Think again. The witch catches you off guard and curses you, causing you to die in the process of indecision. Pathetic.")
+
+        else:
+            print("Hmm? Think again. The cave collapses on you and you die. Pathetic.")
+    else:
+        print("Hmm? Think again. You die in the process of indecision.")
+
+
+
+
+else:
+    print("Hmm? Not a valid choice. You get cursed and die by a thundering lightning strike. An unheroic end to a pathetic adventure.")
