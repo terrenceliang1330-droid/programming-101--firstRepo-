baf61af715e9c1c553cbcbc44b312d676c0a4a8f
Terrence <terrenceliang1330@gmail.com>
2026-05-16 12:28:32 -0400

    final, tuples

--- BASICS/data_struct/tuples.py
+++ BASICS/data_struct/tuples.py
@@ -11,22 +11,22 @@ while True:
    ChooseLocation = input("What coordinates would you like to visit? The locations are: John Pork's House, The Town, The Empire State Building, New York City, Toronto, and Paris. ").lower()
 
    if ChooseLocation == "john pork's house":
-      print(f"You have chosen to visit John Pork's House, which is located at {johnPorkHouseCoord}.")
+      print(f"You have chosen to visit John Pork's House, which is located at the coordinates {johnPorkHouseCoord}.")
       break
    elif ChooseLocation == "the town":
-      print(f"You have chosen to visit The Town, which is located at {townCoord}.")
+      print(f"You have chosen to visit The Town, which is located at the coordinates {townCoord}.")
       break
    elif ChooseLocation == "the empire state building":
-      print(f"You have chosen to visit The Empire State Building, which is located at {empireStateBuildingCoord}.")
+      print(f"You have chosen to visit The Empire State Building, which is located at the coordinates {empireStateBuildingCoord}.")
       break
    elif ChooseLocation == "new york city":
-      print(f"You have chosen to visit New York City, which is located at {newYorkCityCoord}.")
+      print(f"You have chosen to visit New York City, which is located at the coordinates {newYorkCityCoord}.")
       break
    elif ChooseLocation == "toronto":
-      print(f"You have chosen to visit Toronto, which is located at {torontoCoord}.")
+      print(f"You have chosen to visit Toronto, which is located at the coordinates {torontoCoord}.")
       break
    elif ChooseLocation == "paris":
-      print(f"You have chosen to visit Paris, which is located at {parisCoord}.")
+      print(f"You have chosen to visit Paris, which is located at the coordinates {parisCoord}.")
       break
    else:
       print("That is not a valid location. Please choose from the list provided or check your spelling and try again.") 
